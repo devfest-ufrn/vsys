9b3fdc59733e33ee672a6b3469dc42f0dfccc016
Yuri Alessandro Martins <yuri.alessandro@hotmail.com>
2017-10-19 15:48:16 -0300

    Integrate prototype with pyramid #2

--- vsys/__init__.py
+++ vsys/__init__.py
@@ -7,6 +7,11 @@ def main(global_config, **settings):
     config = Configurator(settings=settings)
     config.include('pyramid_jinja2')
     config.add_static_view('static', 'static', cache_max_age=3600)
-    config.add_route('home', '/')
+    
+    config.add_route('index', '/')
+    config.add_route('rentacar', '/rentacar')
+    config.add_route('offercar', '/offercar')
+
+    # config.scan('routes.py')
     config.scan()
     return config.make_wsgi_app()
