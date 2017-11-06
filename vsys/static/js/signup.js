3cce596316a273d8b55e17cbe7ad256910074971
Yuri Alessandro Martins <yuri.alessandro@hotmail.com>
2017-11-06 18:36:47 -0300

    Add page, js and css

---
+++ vsys/static/js/signup.js
@@ -0,0 +1,46 @@
+function hasSpecialCharacters(str) {
+    var expr = /[*\\/|<>\"?:]/g;
+    if (str.search(expr) > 0)
+        return true;
+    else
+        return false;
+}
+
+function validate(fieldname) {
+    var field = document.getElementById(fieldname);
+    var message = document.getElementById(fieldname + "Mes");
+    message.style.display = "inline-block";
+    
+    if (field.value == "") {
+        message.innerHTML = "This field must be filled";
+    } else if (field.value.includes(" ")) {
+        message.innerHTML = "No spaces are allowed in this field";
+    } else if (hasSpecialCharacters(field.value)) {
+        message.innerHTML = "No special characters are allowed on this field";
+    } else if (fieldname == "email" && field.value.indexOf("@") == -1) {
+        message.innerHTML = "Please input a valid email";
+    } else {
+        message.innerHTML = "✓";
+        message.style.color = "green";
+        return true;
+    }
+    message.style.color = "red";
+    return false;
+}
+
+function checkPassword() {
+    var pass = document.getElementById("password");
+    var cpass = document.getElementById("cpassword");
+    var passmes = document.getElementById("cpasswordMes");
+    passmes.style.display = "inline-block";
+    
+    if (cpass.value != pass.value) {
+        passmes.innerHTML = "Passwords don't match";
+        passmes.style.color = "red";
+    } else {
+        passmes.innerHTML = "✓";
+        passmes.style.color = "green";
+    }
+    
+    return cpass.value == pass.value;
+}
